c62f83f7ee9b63eef3c1808581013340bac8ec94
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-08-02 18:03:48 +0300

    hw-03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,54 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import LoadMoreBtn from './LoadMoreBtn';
+import Loader from './Loader';
+
+const perPage = 12;
+const API_KEY = '37391031-3c842063259c869251b7769d0';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    galleryItems: [],
     page: 1,
+    loadMore: false,
+    status: 'idle',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { page, searchQuery } = this.state;
+    if (prevState.searchQuery !== searchQuery) {
+      this.resetPage();
+    }
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ status: 'pending' });
+      try {
+        const response = await fetch(
+          `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+        );
+        const parseResponse = await response.json();
+        this.setState(
+          {
+            galleryItems: parseResponse.hits,
+            status: 'resolved',
+            loadMore: page < Math.ceil(parseResponse.totalHits / 12),
+          },
+          () => {
+            console.log('New status:', this.state.galleryItems); // Log the updated status
+          }
+        );
+      } catch (error) {
+        this.setState({ status: 'rejected' });
+      }
+    }
+  }
+
+  resetPage = () => {
+    this.setState({ page: 1 });
   };
 
   incrementPage = () => {
@@ -16,31 +58,52 @@ export class App extends Component {
   };
 
   handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1 });
+    this.setState({ searchQuery });
   };
 
   render() {
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-        <ImageGallery
-          searchQuery={this.state.searchQuery}
-          page={this.state.page}
-          incrementPage={this.incrementPage}
-        />
-      </div>
-    );
+    const { loadMore, status, galleryItems, searchQuery } = this.state;
+    if (status === 'idle' || status === 'resolved') {
+      return (
+        <>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </>
+      );
+    }
+    if (status === 'pending') {
+      return <Loader />;
+    }
+
+    if (status === 'rejected') {
+      return toast.error('Oops, something went wrong! Please, try again!');
+    }
+
+    if (status === 'resolved') {
+      if (galleryItems.length === 0) {
+        return toast.error(
+          `Sorry, there are no images matching search query ${searchQuery}. Please try again.`
+        );
+      }
+      if (galleryItems.length > 0) {
+        return (
+          <>
+            <ImageGallery galleryItems={galleryItems} />
+            {loadMore && <LoadMoreBtn onClick={this.incrementPage} />}
+          </>
+        );
+      }
+    }
   }
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,56 +1,13 @@
 import { Component } from 'react';
-import { toast } from 'react-toastify';
 import css from './ImageGallery.module.css';
-import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import Loader from 'components/Loader';
-import LoadMoreBtn from 'components/LoadMoreBtn';
 import Modal from 'components/Modal';
-
-const perPage = 12;
-const API_KEY = '37391031-3c842063259c869251b7769d0';
-const BASE_URL = 'https://pixabay.com/api/';
 
 class ImageGallery extends Component {
   state = {
-    galleryItems: [],
-    status: null,
-    page: 1,
-    loadMore: false,
     showModal: false,
+    largeImageURL: null,
   };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { page, searchQuery } = this.props;
-    if (prevProps.searchQuery !== searchQuery) {
-      this.resetPage();
-    }
-    if (prevProps.searchQuery !== searchQuery || prevProps.page !== page) {
-      this.setState({ status: 'pending' });
-      try {
-        const response = await fetch(
-          `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-        );
-        const parseResponse = await response.json();
-        this.setState(prevState => ({
-          galleryItems: [...prevState.galleryItems, ...parseResponse.hits],
-          status: 'resolved',
-          loadMore: page < Math.ceil(parseResponse.totalHits / 12),
-        }));
-      } catch (error) {
-        this.setState({ status: 'rejected' });
-      }
-    }
-  }
-
-  handleIncrementPage = () => {
-    const { incrementPage } = this.props;
-    incrementPage();
-  };
-
-  resetPage() {
-    this.setState({ galleryItems: [], page: 1 });
-  }
 
   handleImageClick = largeImageURL => {
     this.setState({
@@ -66,55 +23,32 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { galleryItems, status, loadMore, showModal } = this.state;
-
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return toast.error('Oops, something went wrong! Please, try again!');
-    }
-
-    if (status === 'resolved') {
-      if (galleryItems.length === 0) {
-        const { searchQuery } = this.props;
-        return toast.error(
-          `Sorry, there are no images matching search query ${searchQuery}. Please try again.`
-        );
-      }
-      return (
-        <>
-          <ul className={css.gallery}>
-            {galleryItems.map(({ id, webformatURL, tags, largeImageURL }) => {
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  smallPicture={webformatURL}
-                  alt={tags}
-                  onClick={this.handleImageClick}
-                  largeImage={largeImageURL}
-                />
-              );
-            })}
-          </ul>
-          {showModal && (
-            <Modal
-              largeImage={this.state.largeImageURL}
-              onModalClose={this.onModalClose}
-            />
-          )}
-          {loadMore && <LoadMoreBtn onClick={this.handleIncrementPage} />}
-        </>
-      );
-    }
+    const { galleryItems } = this.props;
+    const { showModal } = this.state;
+    return (
+      <>
+        <ul className={css.gallery}>
+          {galleryItems.map(({ id, webformatURL, tags, largeImageURL }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                smallPicture={webformatURL}
+                alt={tags}
+                onClick={this.handleImageClick}
+                largeImage={largeImageURL}
+              />
+            );
+          })}
+        </ul>
+        {showModal && (
+          <Modal
+            largeImage={this.state.largeImageURL}
+            onModalClose={this.onModalClose}
+          />
+        )}
+      </>
+    );
   }
 }
 
 export default ImageGallery;
-
-ImageGallery.propTypes = {
-  searchQuery: PropTypes.string,
-  page: PropTypes.number.isRequired,
-  incrementPage: PropTypes.func.isRequired,
-};
